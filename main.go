9870b67f7a6ad859441bf737e312c1a9039ab078
Karim ElNaggar <karim.elnaggar@careem.com>
2022-03-24 17:17:03 +0200

    Created our first web server

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<H1>Welcome to my awesome site!</H1>")
+}
 
 func main() {
-	fmt.Println("Hello, world!")
+	http.HandleFunc("/", handlerFunc)
+	http.ListenAndServe(":3000", nil)
 }
