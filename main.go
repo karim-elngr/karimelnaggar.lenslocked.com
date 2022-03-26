01cb413f5dcfd3878f98496a5190265294e5aa00
Karim ElNaggar <karim.elnaggar@careem.com>
2022-03-26 12:36:24 +0200

    Added FAQ & custom 404 pages

--- main.go
+++ main.go
@@ -11,6 +11,16 @@ func home(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<H1>Welcome to my awesome site!</H1>")
 }
 
+func faq(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprintln(w, "<H1>FAQ</H1>")
+	fmt.Fprintln(w,
+		"<ul><li>How to learn web dev with golang? <em>Watch calhoun."+
+			"io courses"+
+			"</em></li></ul>")
+
+}
+
 func contact(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "<p>To get in touch please send an email to <a href"+
@@ -19,9 +29,17 @@ func contact(w http.ResponseWriter, r *http.Request) {
 		"com</a>.</p>")
 }
 
+func notFound(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprint(w, "<p>Page is not found</p>")
+}
+
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
+	r.HandleFunc("/faq", faq)
 	r.HandleFunc("/contact", contact)
+	r.NotFoundHandler = http.HandlerFunc(notFound)
 	http.ListenAndServe(":3000", r)
 }
